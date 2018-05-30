b9f3da5eeb2233a704ca2a7c07201bd730235222
gleibson <gleibsonglas@gmail.com>
2018-05-30 21:00:24 +0200

    Trying to implement the class concept C++

--- guess_number.cpp
+++ guess_number.cpp
@@ -1,72 +1,53 @@
 #include<iostream>
-#include<time.h>
-#include<cstdlib>
+#include "guess.h"
 
-/*
-void game::test_num(int g, int n) 
-{
-    srand(time(0));
-    number = rand() % 1000 + 1;
-
-    std::cout << number; 
-
-    //std::cout << "Enter the number: ";
-    //std::cin >> guess;
-    /*do {
-        
-        if (guess < number)
-        {
-            std::cout << "Is less than this" << std::endl;
-        }
-        else if (guess > number)
-        {
-            std::cout << "Is more than this" << std::endl;
-
-        }
-        else
-        {
-            std::cout << "This is the number" << std::endl;
-        }
-    } while (guess != number);
-   
-}*/
 
 int main()
 {   
-    int number;
-    int guess;
     
-    //Without that the function rand always will choose the same number 
-    //Function to make the computer choose a number between 1 and 1000
-    srand(time(0)); 
-    number = rand() % 1000 + 1; 
-
-    std::cout << number << std::endl;
-
-    do { //conditional do while the guess 
-
-        std::cout << "Enter the number: "; //loop which stop when the guess number is correct
-        std::cin >> guess;
-
-        if (guess < number)
-        {
-            std::cout << "Is more than this" << std::endl;
-        }
-        else if (guess > number)
-        {
-            std::cout << "Is less than this" << std::endl;
-
-        }
-        else
-        {
-            std::cout << "This is the number" << std::endl;
-        }
-       
-    } while (guess != number);
-    system("PAUSE");
-    //function which push pause message to the system to stop the conditionals
+    game();
     
     return 0;
 
 }
 
+game::game()
+{
+    //Without that the function rand always will choose the same number 
+    //Function to make the computer choose a number between 1 and 1000
+    srand(time(0));
+    number = rand() % 1000 + 1;
+
+    //std::cout << number << std::endl; to test  
+
+    do {  //conditional do while the guess is correct, which is the same value of the number
+
+        std::cout << "Enter the number guess between 1 and 1000: ";
+        std::cin >> guess;
+
+        if (guess < number)  //loop conditianal which stop when the guess number is correct
+        {
+            std::cout << "Is more than this" << std::endl;
+        }
+        else if (guess > number)
+        {
+            std::cout << "Is less than this" << std::endl;
+
+        }
+        else if (guess < 1)
+        {
+            std::cout << "The value to guess is between 1 and 1000" << std::endl;
+        }
+        else if (guess > 1000)
+        {
+            std::cout << "The value to guess is between 1 and 1000" << std::endl;
+
+        }
+        else
+        {
+            std::cout << "This is the number" << std::endl;
+        }
+    } while (guess != number);
+    system("PAUSE");
+    //function which push pause message to the system to stop the conditionals
+}
