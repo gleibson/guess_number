662fbd931d8160859fd5a7c2b397ad3fff2a1b42
gleibson <gleibsonglas@gmail.com>
2018-05-30 20:41:13 +0200

    Header file

---
+++ guess.h
@@ -0,0 +1,17 @@
+#include<time.h>
+#include<time.h>
+#include<cstdlib>
+
+#pragma once
+
+class game  //class called game
+{
+
+private:
+    int guess, number;  // two variables on private scope
+
+public:
+    game();
+       
+};
+
