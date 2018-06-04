9ed30344ea5403a57caa425e6539ca7b6d7351d7
gleibson <gleibsonglas@gmail.com>
2018-06-04 18:33:43 +0200

    pointers

--- guess_number.cpp
+++ guess_number.cpp
@@ -18,6 +18,8 @@ Game::Game()
     srand(time(0));
     number = rand() % 1000 + 1;
 
+    int *ptrguess = &guess; // pointer to guess adress
+    int *ptrnumber = &number; // pointer to number adress
     //std::cout << number << std::endl; to test  
 
     do {  //conditional do while the guess is correct, which is the same value of the number
@@ -27,27 +29,37 @@ Game::Game()
 
         if (guess < number)  //loop conditianal which stop when the guess number is correct
         {
+            std::cout << ptrguess << "  Guess adress " << std::endl; //to show the address of the guess variable
             std::cout << "Is more than this" << std::endl;
         }
         else if (guess > number)
         {
+            std::cout << ptrguess << "  Guess adress" << std::endl;
             std::cout << "Is less than this" << std::endl;
 
         }
         else if (guess < 1)
         {
+            std::cout << ptrguess << "  Guess adress" << std::endl;
             std::cout << "The value to guess is between 1 and 1000" << std::endl;
         }
         else if (guess > 1000)
         {
+            std::cout << ptrguess << "  Guess adress" << std::endl;
             std::cout << "The value to guess is between 1 and 1000" << std::endl;
 
         }
         else
         {
+            std::cout << ptrguess << "  Guess adress" << std::endl;
+            std::cout << ptrnumber << "  Number adress" << std::endl;
             std::cout << "This is the number" << std::endl;
+
+            //std::cout << "Do want play again? Type [Yes] to play again or [No] to finish" << std::endl;
+            //std::cin >> again; 
         }
     } while (guess != number);
     system("PAUSE");
     //function which push pause message to the system to stop the conditionals
 }
+
