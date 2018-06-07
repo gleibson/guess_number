4d7984864a4d101e05a25fa308bf3ac59320dd45
gleibson <gleibsonglas@gmail.com>
2018-06-07 11:11:44 +0200

    Corrections and Maninpulating memory address

--- guess_number.cpp
+++ guess_number.cpp
@@ -21,45 +21,49 @@ Game::Game()
     srand(time(0));
     number = rand() % 1000 + 1;
 
+    int* ptrguessini = &guess;
     int* ptrguess = &guess; // pointer to guess adress
     int* ptrnumber = &number; // pointer to number adress
     //std::cout << number << std::endl; to test  
 
+    std::cout << "Guess memory address " << ptrguessini << std::endl;
+    
     do {  //conditional do while the guess is correct, which is the same value of the number
 
         std::cout << "Enter the number guess between 1 and 1000: ";
         std::cin >> guess;
-
+        std::cout << std::endl << "Guess Memory Address " << ptrguess << std::endl; //to show the address of the guess variable
+            
         if (guess < number)  //loop conditianal which stop when the guess number is correct
         {
-            std::cout << ptrguess << "  Guess adress " << std::endl; //to show the address of the guess variable
-            std::cout << "Is more than this" << std::endl;
+            std::cout << "Is more than " << guess << std::endl;
+
+            for (int i = 0; i < guess; i++)
+            {
+                ptrguess++;
+            }
         }
         else if (guess > number)
         {
-            std::cout << ptrguess << "  Guess adress" << std::endl;
-            std::cout << "Is less than this" << std::endl;
+            
+            std::cout << "Is less than " << guess << std::endl;
+            
+            for (int i = 0; i < guess; i++)
+            {
+                ptrguess++;
+            }
 
         }
-        else if (guess < 1)
+        
+       else
         {
-            std::cout << ptrguess << "  Guess adress" << std::endl;
-            std::cout << "The value to guess is between 1 and 1000" << std::endl;
-        }
-        else if (guess > 1000)
-        {
-            std::cout << ptrguess << "  Guess adress" << std::endl;
-            std::cout << "The value to guess is between 1 and 1000" << std::endl;
-
-        }
-        else
-        {
-            std::cout << ptrguess << "  Guess adress" << std::endl;
             std::cout << ptrnumber << "  Number adress" << std::endl;
             std::cout << "This is the number" << std::endl;
 
         }
     } while (guess != number);
+    std::cout << "Guess final memory address " << ptrguess << std::endl;
+    std::cout << "Guess inital memory address " << ptrguessini << std::endl;
     
     std::cout << "Do want play again? press any key" << std::endl;
     std::cin >> again;
@@ -67,6 +71,7 @@ Game::Game()
 
     system("PAUSE"); //function which push pause message to the system to stop the conditionals
 
+   
 }
 /*
 void test(char a)
